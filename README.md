4802103ab2b29f829e555f701693671dbd366056
Manoel Campos <manoelcampos@gmail.com>
2016-02-28 23:42:09 +0000

    Exemplos movidos para nova branch, separando os do código fonte do projeto. Assim, o pacote manoelcampos/retornoboleto foi incluído como dependência dos exempos e pode ser baixado via Composer.

--- README.md
+++ README.md
@@ -1,35 +1,5 @@
-Retorno-BoletoPHP
------------------
-Biblioteca de classes em PHP 5.5+ para leitura de arquivos de retorno de títulos de cobrança de bancos brasileiros.
+Exemplos do Retorno-BoletoPHP
+-----------------------------
+Diversos exemplos são disponibilizados junto com o projeto, podendo ser acessados a partir do arquivo [index.html](index.html). Para executar os exemplos, é preciso ter o [Composer](http://getcomposer.org) instalado, pois o projeto agora utiliza tal gerenciador de dependência. Com o Composer instalado, em um terminal na pasta raiz do projeto, basta executar `composer install` para baixar o Retorno-BoletoPHP para ser utilizado pelos exemplos.
 
-O projeto utiliza Design Patterns para permitir a fácil extensão, para adição de novos padrões de arquivos de retorno.
-
-A implementação do projeto para versões do PHP inferiores a 5.5 estão obsoletas, não sendo recomendado seu uso. Tal versão obsoleta também não é mais suportada. Se de qualquer maneira desejar tal versão, pode baixar os fonte na [branch php53](https://github.com/manoelcampos/Retorno-BoletoPHP/tree/php53).
-
-Documentação
-------------
-A documentação do projeto foi gerada com [PhpDoc](http://phpdoc.org) e está disponível no diretório [doc](doc/index.html). 
-O PhpDoc está incluído como dependência de desenvolvimento do projeto. Assim, para intalá-lo via composer, basta executar
-`composer install`. Com isto, para gerar a documentação, basta executar `vendor/bin/phpdoc`. Toda a configuração para geração da documentação está definida no arquivo [phpdoc.dist.xml](phpdoc.dist.xml).
-
-Exemplos
---------
-Diversos exemplos são disponibilizados junto com o projeto, podendo ser acessados a partir do arquivo [exemplos/index.html](exemplos/index.html). Para executar os exemplos, é preciso ter o [Composer](http://getcomposer.org) instalado, pois o projeto agora utiliza o tal tal gerenciador de dependência. Com o Composer instalado, em um terminal na pasta raiz do projeto, basta executar `composer install` para instalar as dependências necessárias e gerar o arquivo vendor/autoload.php para permitir fazer o autoload das classes do projeto.
-
-Aviso Legal
------------
-O projeto é disponibilizado "como está" e nenhuma garantia legal é fornecida. Nenhuma responsabilidade será atribuída ao desenvolvedor por danos e prejuízos que por ventura possam ter sido decorridos do uso do projeto, ficando este completamente isento
-de qualquer responsabilidade.
-
-Todo o processo de geração de boletos bancários e leitura de arquivos de retorno deve ser homologado junto ao banco conveniado para assegurar que está tudo ocorrendo dentro dos padrões. Mesmo existindo os padrões FEBRABAN CNAB240 E CNAB400, existem diferentes versões
-de tais padrões e alguns bancos podem utilizar certos campos do arquivo de retorno enquanto outros não. Por isso, é extremamente recomendável que o desenvolvedor, ou o cliente para qual está desenvolvendo o software, entre em contato com o banco para realizar o processo de homologação. 
-
-Use o projeto por sua conta e risco.
-
-Fórum
------
-Dúvidas, acesse o fórum de discussão no [Google Groups](http://groups.google.com/group/retorno-boletophp).
-
-Licença
--------
-Os direitos sobre uso do projeto estão protegidos pela [Licença MIT](LICENSE).
+Para mais informações sobre o projeto, veja a [a branch master com os fontes do projeto](/manoelcampos/Retorno-BoletoPHP/tree/master).
---
+++ exemplo_cnab240_bb.php
@@ -0,0 +1,89 @@
+<?php
+
+/** 
+ * Exemplo de uso da classe para processamento de arquivo de retorno de cobranças em formato FEBRABAN/CNAB240,
+ * testado com arquivo de retorno do Banco do Brasil.
+ * Cateira 18 variação 19 e carteira 18 variação 27 do Banco do Brasil.
+ * 
+ * @license <a href="https://opensource.org/licenses/MIT">MIT License</a>
+ * @author <a href="http://manoelcampos.com/contact">Manoel Campos da Silva Filho</a>
+ * @package exemplos
+ */
+
+require_once("vendor/autoload.php");
+
+use ManoelCampos\RetornoBoleto\LeituraArquivo;
+use ManoelCampos\RetornoBoleto\RetornoFactory;
+use ManoelCampos\RetornoBoleto\RetornoInterface;
+use ManoelCampos\RetornoBoleto\LinhaArquivo;
+
+/**
+ * Função de callback que será chamada cada vez que uma linha for lida do
+ * arquivo de retorno. Esta versão da função de callback imprime campos
+ * específicos de cada linha lida.
+ * 
+ * @param RetornoInterface $retorno Objeto responsável pela leitura do arquivo de retorno
+ * @param LinhaArquivo $linha Objeto contendo os dados da linha lida
+ */
+$processarLinha1 = function (RetornoInterface $retorno, LinhaArquivo $linha) {
+    if($linha->dados["registro"] == $retorno->getIdHeaderArquivo()){
+        echo "<b>Tipo de Arquivo de Retorno: " . get_class($retorno) . "</b><p/>";
+        echo "<table>\n";
+        echo "<tr><th>Linha</th><th>Nosso Número</th><th>Data Pag</th>".
+             "<th>Valor Título</th><th>Valor Pago</th></tr>\n";
+    }
+    else if($linha->dados["registro"] == $retorno->getIdTrailerArquivo()){
+        echo "</table>\n";
+    }
+    else if($linha->dados["registro"] == $retorno->getIdDetalhe()){ 
+        printf(
+            "<tr><td>%d</td><td>%d</td><td>%s</td><td>%.2f</td><td>%.2f</td></tr>\n",
+            $linha->numero, 
+            $linha->dados['nosso_numero'], 
+            $linha->dados["data_pagamento"],
+            $linha->dados["valor_titulo"],
+            $linha->dados["valor_pagamento"]);
+
+        echo "</tr>\n";
+    }
+};
+
+/**
+ * Função de callback que será chamada cada vez que uma linha for lida do
+ * arquivo de retorno. Esta versão da função de callback imprime todos os campos
+ * da linha lida.
+ * 
+ * @param RetornoInterface $retorno Objeto responsável pela leitura do arquivo de retorno
+ * @param LinhaArquivo $linha Objeto contendo os dados da linha lida
+ */
+$processarLinha2 = function (RetornoInterface $retorno, LinhaArquivo $linha) {
+    if($linha->dados["registro"] == $retorno->getIdHeaderArquivo()){
+        echo "<b>Tipo de Arquivo de Retorno: " . get_class($retorno) . "</b><p/>";
+        echo "<table>\n";
+    }
+    else if($linha->dados["registro"] == $retorno->getIdTrailerArquivo()){
+        echo "</table>\n";
+    }
+    else {
+        printf("<tr><th colspan='2'>Número da Linha: %08d</th></tr>\n", $linha->numero);
+        foreach ($linha->dados as $nome_campo => $valor_campo){
+            printf("<tr><td><b>%s</b></td><td>%s</td>\n ", $nome_campo, $valor_campo);
+        }
+        echo "</tr>\n";
+    }
+};
+
+//--------------------------INÍCIO DA EXECUÇÃO DO CÓDIGO------------------------
+$fileName = "retornos/retorno_cnab240-bb.ret";
+
+$cnab240 = RetornoFactory::getRetorno($fileName);
+
+/*
+ Use uma das duas instrucões abaixo para usar uma das duas funções
+ de callback definidas acima (comente uma e descomente a outra).
+*/
+$leitura = new LeituraArquivo($processarLinha1, $cnab240);
+//$leitura = new LeituraArquivo($processarLinha2, $cnab240);
+
+$leitura->lerArquivoRetorno();
+
